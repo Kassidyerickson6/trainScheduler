e86c7373036fa770824561dce450ff54c68d8e50
Kassi Erickson <you@example.com>
2017-12-12 19:28:39 -0700

    final touches

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,24 @@
+// The page runs once the DOM is ready for Javascript
 $(document).ready(function(){
-  // Link to Firebase
-  var trainData = new Firebase("https://train-scheduler-45f2e.firebaseio.com/");
+  // This links to firebase
+  var Data = new Firebase("https://train-scheduler-45f2e.firebaseio.com/");
 
-
+// button for adding a train
   $("#add-train").on("click", function(){
 
-
+// variables created for user input
     var trainName = $("#name-input").val().trim();
     var destination = $("#destination-input").val().trim();
     var trainTime = moment($("#time-input").val().trim(), "HH:mm").subtract(10, "years").format("X");;
     var frequency = $("#frequency-input").val().trim();
 
+//logs the name, destination, time, and frequency in the console
     console.log(trainName);
     console.log(destination);
     console.log(trainTime);
     console.log(frequency);
 
-    // Creates local "temporary" object for holding train data
-    // Will push this to firebase
+// pushing this information to firebase
     var newTrain = {
      name:trainName,
       destination: destination,
@@ -25,44 +26,53 @@ $(document).ready(function(){
       frequency: frequency,
     }
 
-    // pushing trainInfo to Firebase
-    trainData.push(newTrain);
+    Data.push(newTrain);
 
-    // clear text-boxes
+    // clears the text-boxes and sets empty strings
     $("#name-input").val("");
     $("#destination-input").val("");
     $("#time-input").val("");
     $("#frequency-input").val("");
 
-    // Prevents page from refreshing
+    // Keeps the current input information on the page
     return false;
   });
 
-  trainData.on("child_added", function(childSnapshot, prevChildKey){
+//function created with selector for childSnapshot
+// child_added retrieves a list of items from the database
+  Data.on("child_added", function(childSnapshot, prevChildKey){
 
     console.log(childSnapshot.val());
 
-    // assign firebase variables to snapshots.
-    var firebaseName = childSnapshot.val().name;
-    var firebaseDestination = childSnapshot.val().destination;
-    var firebaseTrainTimeInput = childSnapshot.val().trainTime;
-    var firebaseFrequency = childSnapshot.val().frequency;
-    
-    var diffTime = moment().diff(moment.unix(firebaseTrainTimeInput), "minutes");
-    var timeRemainder = moment().diff(moment.unix(firebaseTrainTimeInput), "minutes") % firebaseFrequency ;
-    var minutes = firebaseFrequency - timeRemainder;
+    // firebase variables attached to the snapshots
+    var fbName = childSnapshot.val().name;
+    var fbDestination = childSnapshot.val().destination;
+    var fbTime = childSnapshot.val().trainTime;
+    var fbFrequency = childSnapshot.val().frequency;
 
-    var nextTrainArrival = moment().add(minutes, "m").format("hh:mm A"); 
+    // moment.js
+    //.diff difference
+    // unix timestamp
+    var diffTime = moment().diff(moment.unix(fbTime), "minutes");
+    var timeRemainder = moment().diff(moment.unix(fbTime), "minutes") % fbFrequency ;
+    var minutes = fbFrequency - timeRemainder;
+
+    var nextArrival = moment().add(minutes, "m").format("hh:mm A"); 
     
-    // Test for correct times and info
+    
     console.log(minutes);
-    console.log(nextTrainArrival);
+    console.log(nextArrival);
     console.log(moment().format("hh:mm A"));
-    console.log(nextTrainArrival);
+    console.log(nextArrival);
     console.log(moment().format("X"));
 
-    // Append train info to table on page
-    $("#trainTable > tbody").append("<tr><td>" + firebaseName + "</td><td>" + firebaseDestination + "</td><td>" + firebaseFrequency + " mins" + "</td><td>" + nextTrainArrival + "</td><td>" + minutes + "</td></tr>");
+    // train info is appended to the page
+    $("#trainTable > tbody").append("<tr><td>" 
+      + fbName + "</td><td>" + fbDestination + "</td><td>" 
+      + fbFrequency + " mins" + "</td><td>" 
+      + nextArrival + "</td><td>" + minutes + "</td></tr>");
 
   });
 });
+
+
